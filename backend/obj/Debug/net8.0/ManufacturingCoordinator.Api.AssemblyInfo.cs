91843c86da1fa5bc83f761dc862e092c41b8fb89
ChandranSukirthan <sukirsukirthan347@gmail.com>
2026-08-14 10:47:35 +0530

    Check build

--- backend/obj/Debug/net8.0/ManufacturingCoordinator.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/ManufacturingCoordinator.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f62d077e96e20adba4a2e94522cd6b956ff089")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
