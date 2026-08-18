2a1dd713e791c3e1eb950f9476e0a5e46f490c10
ChandranSukirthan <sukirsukirthan347@gmail.com>
2026-08-19 00:55:03 +0530

    Update binary files and assembly information for ManufacturingCoordinator.Api

--- backend/obj/Debug/net8.0/ManufacturingCoordinator.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/ManufacturingCoordinator.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa118d2c41d5357a7e17a45e48fb630abd832072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10c3ae74fca60cee65c8b8f8175858e64e3da86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
