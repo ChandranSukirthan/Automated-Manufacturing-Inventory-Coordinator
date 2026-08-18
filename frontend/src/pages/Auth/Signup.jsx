3e12a579c436b84414b422574906ef1c4d50cd9f
ChandranSukirthan <sukirsukirthan347@gmail.com>
2026-08-18 16:34:53 +0530

    Change the placeholder

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -74,7 +74,7 @@ export default function Signup() {
               value={formData.name}
               onChange={handleChange}
               className="w-full pl-10 pr-4 py-2.5 bg-slate-900/50 border border-slate-700 rounded-xl focus:ring-2 focus:ring-brand-500 focus:border-brand-500 transition-all text-white placeholder-slate-500"
-              placeholder="John Doe"
+              placeholder="Enter your FullName"
             />
           </div>
         </div>
@@ -91,7 +91,7 @@ export default function Signup() {
               value={formData.email}
               onChange={handleChange}
               className="w-full pl-10 pr-4 py-2.5 bg-slate-900/50 border border-slate-700 rounded-xl focus:ring-2 focus:ring-brand-500 focus:border-brand-500 transition-all text-white placeholder-slate-500"
-              placeholder="john@amic.com"
+              placeholder="Enter your email"
             />
           </div>
         </div>
