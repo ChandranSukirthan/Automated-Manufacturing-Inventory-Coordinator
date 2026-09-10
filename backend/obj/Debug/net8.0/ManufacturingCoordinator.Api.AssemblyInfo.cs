13689aa9b7a1afa57ad03fb5b11ebca0b21433c3
Nithushan Uthayarasa <uthayarasanithushan103@gmail.com>
2026-09-10 15:07:28 +0530

    Secure QA role assignment

--- backend/obj/Debug/net8.0/ManufacturingCoordinator.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/ManufacturingCoordinator.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6ffe5c7f681336a2d95119ea8369afa25efa02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f9a8df0b6e5df5c887cad2e67c7b9c929a9080")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManufacturingCoordinator.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
